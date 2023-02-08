b0dd5a8885cf0be74af36b0c7778f36b9c5c6c97
Craftzman7 <zezetta@icloud.com>
2023-02-07 22:08:05 -0800

    Hooray!

---
+++ src/components/app.js
@@ -0,0 +1,16 @@
+import { Router } from 'preact-router';
+
+// Code-splitting is automated for `routes` directory
+import Home from '../routes/home';
+
+const App = () => (
+	<div id="app">
+		<main>
+			<Router>
+				<Home path="/" />
+			</Router>
+		</main>
+	</div>
+);
+
+export default App;
