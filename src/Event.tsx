442e699d5ac148867ec3502d186c80fc0818e2ef
Craftzman7 <zezetta@icloud.com>
2023-02-18 21:32:26 -0800

    dynamically render EVERYTHING
    Co-authored-by: Max Niederman <max@maxniederman.com>

--- src/Event.tsx
+++ src/Event.tsx
@@ -1,71 +1,59 @@
-import { FunctionComponent } from 'preact';
-import { useEffect, useState } from 'preact/hooks';
-import { Suspense, lazy } from 'preact/compat';
+import { useEffect, useState } from "preact/hooks";
 
 const DAYS = 1000 * 60 * 60 * 24;
 const HOURS = 1000 * 60 * 60;
 const MINUTES = 1000 * 60;
 const SECONDS = 1000;
 
-export default function Event(props: { date: Date; name: string }) {
+export default function Event(props: {
+    date: Date;
+    name: string;
+}) {
     return (
         <div class="flex flex-col gap-4 border p-4 rounded-lg m-5">
             <h2 class="text-4xl md:text-5xl mb-5 underline">{props.name}</h2>
-            <UpcomingDate date={props.date} />
+                <UpcomingDate date={props.date}/>
         </div>
     );
 }
 
-interface CalendarItem {
-    status: string;
-    summary: string;
-    start: {
-        dateTime?: string;
-    };
-}
 
-async function getMeetingsUntil(): Promise<FunctionComponent<{ event: Date }>> {
-    const filterDate = new Date().toISOString();
-    let res = await fetch(
-        `https://www.googleapis.com/calendar/v3/calendars/ghsrobotics3636@gmail.com/events?showDeleted=false&maxResults=9999&orderBy=updated&timeMin=${filterDate}&singleEvents=true`,
-        {
-            headers: {
-                'X-goog-api-key': 'AIzaSyDez4WTShYYZJfCQ3zMJYr-Vp9KGFk-fcI'
-            }
-        }
-    );
-    let data = (await res.json()) as { items: CalendarItem[] };
 
-    return ({ event }) => {
-        let finalMeetings = 0;
-
-        for (const item of data.items) {
-            // the showDeleted parameter filters out most of the cancelled events
-            // but not all of them. it filters out about
-            // 7 kib worth of data and then just gives up on two events
-            if (item.status === 'cancelled') continue;
-
-            // we should ignore non-meetings (e.g. events, etc)
-            // e.g. "Robotics Meeting" -> "robotics meeting" -> contains "meeting"
-            if (!item.summary.toLowerCase().includes('meeting')) continue;
-
-            if (!item.start.dateTime) continue;
-            const meetingTime = new Date(item.start.dateTime).getTime();
+async function MeetingsUntil(date: Date) {
+    let finalMeetings = 0;
+    const filterDate = new Date(Date.now()).toISOString();
+    let res = await fetch(`https://www.googleapis.com/calendar/v3/calendars/ghsrobotics3636@gmail.com/events?showDeleted=false&maxResults=9999&orderBy=updated&timeMin=${filterDate}&singleEvents=true`, {
+        headers: {
+            "X-goog-api-key": "AIzaSyDez4WTShYYZJfCQ3zMJYr-Vp9KGFk-fcI",
+        },
+    })
+    let data = await res.json();
+    // loop through all events
+    for (let i = 0; i < data.items.length; i++) {
+        // the showDeleted parameter filters out most of the cancelled events
+        // but not all of them. it filters out about
+        // 7 kib worth of data and then just gives up on two events
+    
+        if (data.items[i].status == "cancelled")
+            continue;
+        // if the event is a meeting
+        if (data.items[i].summary.toLowerCase().includes("meeting")) {
+            if(data.items[i].start.dateTime == undefined)
+                continue;
+            const meetingTime = new Date(data.items[i].start.dateTime).getTime();
             const currentTime = Date.now();
             // if the event is before the date
-            const eventDate = event.getTime();
+            const eventDate = date.getTime();
             if (meetingTime < eventDate && meetingTime > currentTime) {
                 finalMeetings++;
             }
         }
-
-        return <>{finalMeetings}</>;
-    };
+    }
+    return finalMeetings;
 }
 
-const MeetingsUntil = lazy(getMeetingsUntil);
 
-function UpcomingDate(props: { date: Date }) {
+function UpcomingDate(props: { date: Date; }) {
     const getDifference = () => props.date.getTime() - Date.now();
     const [difference, setDifference] = useState(getDifference());
 
@@ -77,28 +65,32 @@ function UpcomingDate(props: { date: Date }) {
         return () => clearInterval(clock);
     }, []);
 
+    const [meetingsUntil, setMeetingsUntil] = useState(0);
+    useEffect(() => {
+        MeetingsUntil(props.date).then((meetings: number) => {
+            setMeetingsUntil(meetings);
+        });
+    });
+
+
+
     // each variable does not include the last, so e.g. hours is always under 24
     const daysUntil = Math.floor(difference / DAYS);
     const hoursUntil = Math.floor((difference % DAYS) / HOURS);
     const minutesUntil = Math.floor((difference % HOURS) / MINUTES);
     const secondsUntil = Math.floor((difference % MINUTES) / SECONDS);
-
+    
     return (
         <div class="flex flex-col gap-6 text-3xl md:text-4xl">
             <h3 class="flex gap-4">
                 <span class="font-bold flex-shrink-0">T minus:</span>
                 <span>
-                    {daysUntil} days, {hoursUntil} hours, {minutesUntil}{' '}
+                    {daysUntil} days, {hoursUntil} hours, {minutesUntil}{" "}
                     minutes, and {secondsUntil} seconds
                 </span>
             </h3>
             <h3>
-                <Suspense fallback="Loading meetings...">
-                    <span class="font-bold">
-                        <MeetingsUntil event={props.date} />
-                    </span>{' '}
-                    meetings away
-                </Suspense>
+                <span class="font-bold">{meetingsUntil}</span> meetings away
             </h3>
         </div>
     );
--- src/app.tsx
+++ src/app.tsx
@@ -1,23 +1,58 @@
+import { lazy, Suspense, useEffect, useState } from 'preact/compat';
 import Event from './Event';
 
-export const OREGON_FAIRGORUNDS = new Date('Mar 23 2023');
-export const WILSONVILLE = new Date('Mar 9 2023');
+async function getEvents(): Promise<Record<string, any>> {
+    const filterDate = new Date().toISOString();
+    let res = await fetch(
+        `https://www.googleapis.com/calendar/v3/calendars/ghsrobotics3636@gmail.com/events?showDeleted=false&maxResults=9999&orderBy=updated&timeMin=${filterDate}&singleEvents=true`,
+        {
+            headers: {
+                'X-goog-api-key': 'AIzaSyDez4WTShYYZJfCQ3zMJYr-Vp9KGFk-fcI'
+            }
+        }
+    );
+    let data = await res.json();
+
+    let events = {};
+
+    for (const item of data.items) {
+        // the showDeleted parameter filters out most of the cancelled events
+        // but not all of them. it filters out about
+        // 7 kib worth of data and then just gives up on two events
+        if (item.status === 'cancelled') continue;
+
+        if (!item.summary.toLowerCase().includes('event')) continue;
+
+        if (!item.start.dateTime) continue;
+        const eventTime = new Date(item.start.dateTime).getTime();
+        const currentTime = Date.now();
+        if (eventTime > currentTime) {
+            //@ts-ignore
+            events[item.summary] = item;
+        }
+    }
+
+    return events;
+}
+
 
 const App = () => {
+    const [events, setEvents] = useState<any>(null);
+
+    useEffect(() => {
+        getEvents().then(setEvents);
+    });
+
+
     return (
         <div class="flex flex-col items-center justify-center h-full py-20 mb-5">
             <h1 class="text-4xl font-bold md:text-5xl md:font-normal lg:text-8xl my-10">
                 Upcoming 3636 Events
             </h1>
             <div class="flex flex-col lg:flex-row h-full">
-                <Event
-                    date={WILSONVILLE}
-                    name="Wilsonville"
-                />
-                <Event
-                    date={OREGON_FAIRGORUNDS}
-                    name="Oregon Fairgrounds"
-                />
+                {events === null
+                    ? <div>Loading...</div>
+                    : Object.values(events).map((event: any) => <Event name={event.summary} date={new Date(event.start.dateTime ?? "1970-01-01")} />)}
             </div>
         </div>
     );
