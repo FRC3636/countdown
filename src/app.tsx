eb6c219eb659c1b82fab9eef69876240454fea8d
doinkythederp <doinkythederp@icloud.com>
2023-02-20 14:40:45 -0800

    fix: stop spamming the google api

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,12 @@
 import { lazy, Suspense, useEffect, useState } from 'preact/compat';
 import Event from './Event';
 
-async function getEvents(): Promise<Record<string, any>> {
+export const DAYS = 1000 * 60 * 60 * 24;
+export const HOURS = 1000 * 60 * 60;
+export const MINUTES = 1000 * 60;
+export const SECONDS = 1000;
+
+async function getCalandarItems(): Promise<Record<string, any>> {
     const filterDate = new Date().toISOString();
     let res = await fetch(
         `https://www.googleapis.com/calendar/v3/calendars/ghsrobotics3636@gmail.com/events?showDeleted=false&maxResults=9999&orderBy=updated&timeMin=${filterDate}&singleEvents=true`,
@@ -11,8 +16,11 @@ async function getEvents(): Promise<Record<string, any>> {
             }
         }
     );
-    let data = await res.json();
+    if (!res.ok) throw new Error('Error response from google api');
+    return await res.json();
+}
 
+function getEvents(data: Record<string, any>): Record<string, any> {
     let events = {};
 
     for (const item of data.items) {
@@ -35,14 +43,33 @@ async function getEvents(): Promise<Record<string, any>> {
     return events;
 }
 
+/**
+ * Starts an interval, then cancels it when the component is removed
+ *
+ * @param runOnStart - Pass `true` to run the function parameter before starting the interval
+ */
+export function useInterval(
+    effect: () => void,
+    interval: number,
+    runOnStart = false
+) {
+    // overengineered = yes
+    // useEffect wrapper so it only runs once in components
+    useEffect(() => {
+        if (runOnStart) effect();
+
+        const clock = setInterval(effect, interval);
+        // interval stops when the component is removed
+        return () => clearInterval(clock);
+    }, []);
+}
 
 const App = () => {
-    const [events, setEvents] = useState<any>(null);
+    const [calItems, setCalItems] = useState<any>(null);
+    // refetch data every hour to keep up-to-date without reloads
+    useInterval(() => getCalandarItems().then(setCalItems), HOURS * 1, true);
 
-    useEffect(() => {
-        getEvents().then(setEvents);
-    });
-
+    const events = calItems ? getEvents(calItems) : null;
 
     return (
         <div class="flex flex-col items-center justify-center h-full py-20 mb-5">
@@ -50,9 +77,19 @@ const App = () => {
                 Upcoming 3636 Events
             </h1>
             <div class="flex flex-col lg:flex-row h-full">
-                {events === null
-                    ? <div>Loading...</div>
-                    : Object.values(events).map((event: any) => <Event name={event.summary} date={new Date(event.start.dateTime ?? "1970-01-01")} />)}
+                {events === null ? (
+                    <div>Loading...</div>
+                ) : (
+                    Object.values(events).map((event: any) => (
+                        <Event
+                            name={event.summary}
+                            date={
+                                new Date(event.start.dateTime ?? '1970-01-01')
+                            }
+                            calItems={calItems}
+                        />
+                    ))
+                )}
             </div>
         </div>
     );
